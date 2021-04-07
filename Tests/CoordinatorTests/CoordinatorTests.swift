9cc370c83f7be7b3e464db798ea3f9860f82da98
Charles Hsieh <charlous167@gmail.com>
2021-04-07 13:14:45 +0800

    Build fundamental functionalities of Coordinator (#1)
    * Build Coordinator with fundamental functions
    
    * Test completion callback for CoordinatedResult
    
    * Test releasing Coordinator after completion

--- Tests/CoordinatorTests/CoordinatorTests.swift
+++ Tests/CoordinatorTests/CoordinatorTests.swift
@@ -2,14 +2,61 @@ import XCTest
 @testable import Coordinator
 
 final class CoordinatorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Coordinator().text, "Hello, World!")
+    static var allTests = [
+        ("testCoordinatedResult", testCoordinatedResult),
+        ("testRelease", testRelease),
+    ]
+
+    private var rootCoordinator: RootCoordinator!
+
+    override func setUpWithError() throws {
+        rootCoordinator = RootCoordinator()
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    override func tearDownWithError() throws {
+        rootCoordinator = nil
+    }
+
+    func testCoordinatedResult() throws {
+        let promise = expectation(description: "Callback-CoordinatedResult")
+
+        let mockCoordinator = MockCoordinator()
+        rootCoordinator.launch(mockCoordinator) { info in
+            // async
+            XCTAssertEqual(info, String(describing: MockCoordinator.self))
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5.0)
+    }
+
+    func testRelease() throws {
+        let promise = expectation(description: "Coordinator-released-after-completion")
+
+        weak var retainedMockCoordinator: MockCoordinator?
+        var mockCoordinator: MockCoordinator? = MockCoordinator()
+        rootCoordinator.launch(mockCoordinator!) { info in
+            // async
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                // executed after completion block
+                XCTAssertNil(retainedMockCoordinator)
+                promise.fulfill()
+            }
+        }
+        retainedMockCoordinator = mockCoordinator
+        mockCoordinator = nil
+        XCTAssertNotNil(retainedMockCoordinator)
+        wait(for: [promise], timeout: 5.0)
+    }
+}
+
+private final class RootCoordinator: Coordinator<String> {
+    override func start(onCompleted: @escaping (String) -> Void) {}
+}
+
+private final class MockCoordinator: Coordinator<String> {
+    override func start(onCompleted: @escaping (String) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            onCompleted("\(type(of: self))")
+        }
+    }
 }
