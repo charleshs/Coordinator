51a203748cc6e44ec7493b0cc8ecd3c5b9a8374b
Charles Hsieh <charlous167@gmail.com>
2021-04-07 18:12:51 +0800

    Create CoordinatorDemo project

---
+++ CoordinatorDemo/CoordinatorDemo/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  CoordinatorDemo
+//
+//  Created by Charles Hsieh on 2021/4/7.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ CoordinatorDemo/CoordinatorDemo/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  CoordinatorDemo
+//
+//  Created by Charles Hsieh on 2021/4/7.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
