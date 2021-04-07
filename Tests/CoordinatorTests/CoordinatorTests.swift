ccdf2bf0c9c2d1bbc9a38ee53ada9747a7963fa8
Charles Hsieh <charlous167@gmail.com>
2021-04-07 11:52:19 +0800

    Test completion callback for CoordinatedResult

--- Tests/CoordinatorTests/CoordinatorTests.swift
+++ Tests/CoordinatorTests/CoordinatorTests.swift
@@ -3,8 +3,38 @@ import XCTest
 
 final class CoordinatorTests: XCTestCase {
     static var allTests = [
-        ("testExample", testExample),
+        ("testCoordinatedResult", testCoordinatedResult),
     ]
 
-    func testExample() throws {}
+    private var rootCoordinator: RootCoordinator!
+
+    override func setUpWithError() throws {
+        rootCoordinator = RootCoordinator()
+    }
+
+    override func tearDownWithError() throws {
+        rootCoordinator = nil
+    }
+
+    func testCoordinatedResult() throws {
+        let promise = expectation(description: "Callback-CoordinatedResult")
+        let mockCoordinator = MockCoordinator()
+        rootCoordinator.launch(mockCoordinator) { info in
+            XCTAssertEqual(info, String(describing: MockCoordinator.self))
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5.0)
+    }
+}
+
+private final class RootCoordinator: Coordinator<String> {
+    override func start(onCompleted: @escaping (String) -> Void) {}
+}
+
+private final class MockCoordinator: Coordinator<String> {
+    override func start(onCompleted: @escaping (String) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            onCompleted("\(type(of: self))")
+        }
+    }
 }
